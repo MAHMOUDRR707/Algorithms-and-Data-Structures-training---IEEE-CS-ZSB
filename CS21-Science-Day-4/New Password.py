2ad572be9f1e94e5d4a705ad895961824b04467e
mahmoud reda <55753782+MAHMOUDRR707@users.noreply.github.com>
2020-12-12 03:57:33 +0200

    Add files via upload

--- CS21-Science-Day-4/New Password.py
+++ CS21-Science-Day-4/New Password.py
@@ -1,1 +1,8 @@
-
+#problem link :https://codeforces.com/contest/770/problem/A
+#run time :  O(n)
+import random
+n,m = map(int,input().split())
+z = "abcdefghijklmnopqrstuvwxyz"
+for i in range(n):
+ print(z[i%m],end="")
+    
