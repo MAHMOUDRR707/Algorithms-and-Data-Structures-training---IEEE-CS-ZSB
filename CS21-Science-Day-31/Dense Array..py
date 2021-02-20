04c61fbe6a1f4175c9c19412306ca9f1629cd1b1
mahmoud reda <55753782+MAHMOUDRR707@users.noreply.github.com>
2021-02-20 06:01:52 +0200

    Create Dense Array..py

---
+++ CS21-Science-Day-31/Dense Array..py
@@ -0,0 +1,17 @@
+#problem link : https://codeforces.com/contest/1490/problem/A
+#time : O(n*3)
+
+n =  int(input())
+for i in range(n):
+    m =  int(input())
+    y = 0
+    x =  list(map(int,input().split()))
+    for j in range(1,m):
+        m1 = max(x[j],x[j-1])
+        m2 = min(x[j],x[j-1])
+        if (m1 >= (2*m2)):
+          while(m1 > 2*m2):
+            m2*=2
+            y+=1
+    print(y)
+        
